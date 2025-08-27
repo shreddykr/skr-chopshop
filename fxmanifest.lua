5af6e87a26bebbbe2a81efbe269d93674525cd3e
shreddykr <148612874+shreddykr@users.noreply.github.com>
2025-08-27 10:02:11 -0700

    fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,28 +1,21 @@
 fx_version 'cerulean'
 game 'gta5'
 
--- Resource information
 author 'shreddykr'
 description 'Advanced Chopshop Missions'
-version '1.0.6'
+version '1.0.7'
 
--- Client scripts
 client_scripts {
-    'client/*.lua',  -- Mission handling logic
+    'client/*.lua',  
 }
 
--- Server scripts
 server_scripts {
-    'server/*.lua'    -- Main server-side logic
+    'server/*.lua'   
 }
 
--- Shared config file
+
 shared_scripts  {
     'config.lua',
-}   -- Shared configuration file
+}   
 
--- Dependencies (if any)
-dependency 'ox_target'  -- Required for NPC interaction
-dependency 'qb-core'    -- Core QBCore framework for player management and notifications
-
--- This is just a basic fxmanifest, make sure the paths are correct for your scripts and dependencies.
+dependency 'qb-core' 
